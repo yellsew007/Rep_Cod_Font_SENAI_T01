8b85894b319b6bd8577f1ef110249d9a95095f3b
yellsew007 <99208152+yellsew007@users.noreply.github.com>
2022-02-15 13:54:42 -0300

    Atividade Online 2
    Atividade da disciplina lógica de programação.

---
+++ Atividade2.js
@@ -0,0 +1,13 @@
+// Modelo de Cadastro
+console.log ("Seja Bem Vindo!");
+console.log ("Insira sua Idade:");
+if (18 >= 18) console.log ("Cadastro Realizado com Sucesso!");
+else console.log ("Cadastro Negado.")
+
+
+var TotalDeCadastro =0
+while (TotalDeCadastro <=100) {
+     console.log ("Cadastro Realizado com Sucesso!");
+TotalDeCadastro++
+}
+if (TotalDeCadastro >=100) console.log ("Já esgotamos nossas vagas.");

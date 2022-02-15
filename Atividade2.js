18ad11fe5b73c4a2ca410fc8814d14c4b30ce087
yellsew007 <99208152+yellsew007@users.noreply.github.com>
2022-02-15 13:55:24 -0300

    Update Atividade2.js

--- Atividade2.js
+++ Atividade2.js
@@ -4,7 +4,6 @@ console.log ("Insira sua Idade:");
 if (18 >= 18) console.log ("Cadastro Realizado com Sucesso!");
 else console.log ("Cadastro Negado.")
 
-
 var TotalDeCadastro =0
 while (TotalDeCadastro <=100) {
      console.log ("Cadastro Realizado com Sucesso!");
